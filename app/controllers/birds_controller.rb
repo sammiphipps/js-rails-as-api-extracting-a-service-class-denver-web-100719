07823bb266c3a364156bca907f23c0908dede305
Samantha Phipps <sammi.phipps@gmail.com>
2019-10-29 11:24:14 -0600

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,10 @@
 class BirdsController < ApplicationController
+    def index
+        @birds = Bird.all
+        render json: @birds
+    end 
+    def show
+        @bird = Bird.find(params[:id])
+        render json: @bird
+    end 
 end
